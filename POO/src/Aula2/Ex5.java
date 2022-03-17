829c0b446e7f40beb31710c4a58cebd59560a8ba
Rafael <rafaelpbpinto2@gmail.com>
2022-03-17 09:18:53 +0000

    Aula2-POO

--- POO/src/Aula2/Ex5.java
+++ POO/src/Aula2/Ex5.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Ex5 {
     public static final Scanner sc = new Scanner(System.in);
     public static void main(String[] args){
-        double v1, v2, d1, d2, vmedia;
+        double v1, v2, d1, d2,t1, t2, vmedia;
         System.out.print("Velocidade no primeiro trajeto (Km/h): ");
         v1 = sc.nextDouble();
         System.out.print("Distancia percorrida no primeiro trajeto (Km): ");
@@ -12,8 +12,10 @@ public class Ex5 {
         System.out.print("Velocidade no segundo trajeto (Km/h): ");
         v2 = sc.nextDouble();
         System.out.print("Distancia percorrida no segundo trajeto (Km): ");
-        d2 = sc.nextDouble();
-        vmedia = d1+d2 / ((d1/v1)+(d2/v2));
+        d2 = sc.nextDouble();                                                                           //vmedia=d/t
+        t1 = d1 / v1;                                                                                   //t = d/vmedia
+        t2 = d2 / v2;
+        vmedia = (d1 + d2) / (t1 + t2);
         System.out.println("A velocidade media final e " + vmedia + " Km/h.");
     }
 }
