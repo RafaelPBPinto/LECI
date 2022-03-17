afb43aa6174abcdfce99157d7f5410729c61b6d2
Rafael <rafaelpbpinto2@gmail.com>
2022-03-17 15:41:38 +0000

    Aula2-POO final

--- POO/src/Aula2/Ex3.java
+++ POO/src/Aula2/Ex3.java
@@ -12,10 +12,20 @@ public class Ex3 {
                 System.out.println("Valor Invalido! Quantidade tem de ser maior de 0.");
             }
         }while(M < 0);
-        System.out.print("Temperatura inicial da agua (em graus Celsius): ");
-        initialTemperature = sc.nextDouble();
-        System.out.print("Temperatura final da agua (em graus Celsius): ");
-        finalTemperature = sc.nextDouble();
+        do{
+            System.out.print("Temperatura inicial da agua (em graus Celsius): ");
+            initialTemperature = sc.nextDouble();
+            if(initialTemperature < -273.15 || initialTemperature > 100){
+                System.out.println("Valor Invalido!");
+            }
+        }while(initialTemperature < -273.15 || initialTemperature > 100);
+        do{
+            System.out.print("Temperatura final da agua (em graus Celsius): ");
+            finalTemperature = sc.nextDouble();
+            if(finalTemperature < -273.15 || finalTemperature > 100){
+                System.out.println("Valor Invalido!");
+            }
+        }while(finalTemperature < -273.15 || finalTemperature > 100);
         Q = M * (finalTemperature - initialTemperature) * 4184;
         System.out.println("Energia necessaria: " + Q + " Joules");
     }
